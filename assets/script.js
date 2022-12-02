3e15b2a33c2c1c2221f7cae80320cb0c7aa0934c
Ramon Royo <rroyog@uoc.edu>
2022-12-02 21:01:13 +0100

    Project finished

---
+++ assets/script.js
@@ -0,0 +1,99 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+let contentTotalHeight, innerHeight, verticalScroll;
+let imagesToLoad = 3;
+const imagesToLoadAfterFirstLoad = 10;
+let allImagesLoaded = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+const UNSPLASH_PUBLIC_API_ACCESS_KEY = "BeNTmbYEdY9Xlz4gHeI9kuwRWDJAt7eHVk4dImQFGu8";
+let unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_PUBLIC_API_ACCESS_KEY}&count=${imagesToLoad}`;
+
+/**
+ * @dev After the first load, set a higher amount of photos to download
+ */
+function afterFirstLoad() {
+  imagesToLoad = imagesToLoadAfterFirstLoad;
+  unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_PUBLIC_API_ACCESS_KEY}&count=${imagesToLoad}`;
+}
+
+/**
+ * @dev Check if all images were loaded
+ */
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    allImagesLoaded = true;
+    loader.hidden = true;
+    afterFirstLoad();
+  }
+}
+
+/**
+ * @dev
+ */
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+/**
+ * @dev Create Elements for Links & Photos, Add to DOM
+ */
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "blank",
+    });
+    // Create image for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description || "No description available",
+      title: photo.alt_description || "No description available",
+    });
+    // Event Listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+    // Put <img> inside <a>, then put both inside imageContainer
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+/**
+ * @dev Get photos from Unsplash API
+ */
+async function getPhotos() {
+  try {
+    const response = await fetch(unsplashUrl);
+    photosArray = await response.json();
+    totalImages += photosArray.length;
+    console.log(`total images: ${totalImages}`);
+    await displayPhotos();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Check to see if scrolling near the bottom of the page, Load more photos
+window.addEventListener("scroll", () => {
+  contentTotalHeight = document.body.offsetHeight;
+  verticalScroll = window.scrollY;
+  innerHeight = window.innerHeight;
+
+  if (allImagesLoaded && verticalScroll + innerHeight >= contentTotalHeight - 1000) {
+    allImagesLoaded = false;
+    getPhotos();
+  }
+});
+
+// On load
+getPhotos();
